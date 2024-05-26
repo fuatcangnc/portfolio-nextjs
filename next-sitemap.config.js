723114c5f16edb2ea3a7be4fe0fcd2adf55f60c5
Fuat <fuatcankod@gmail.com>
2024-05-26 13:32:05 +0300

    added sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,52 @@
+/** @type {import('next-sitemap').IConfig} */
+
+const config = {
+  siteUrl: "https://fuatcangenc.com",
+  changefreq: "daily",
+  priority: 0.7,
+  sitemapSize: 5000,
+  generateRobotsTxt: true,
+  exclude: ["/studio"],
+  // alternateRefs: [
+  //   {
+  //     href: 'https://es.example.com',
+  //     hreflang: 'es',
+  //   },
+  //   {
+  //     href: 'https://fr.example.com',
+  //     hreflang: 'fr',
+  //   },
+  // ],
+  // Default transformation function
+  transform: async (config, path) => {
+    return {
+      loc: path, // => this will be exported as http(s)://<config.siteUrl>/<path>
+      changefreq: config.changefreq,
+      priority: config.priority,
+      lastmod: config.autoLastmod ? new Date().toISOString() : undefined,
+      alternateRefs: config.alternateRefs ?? [],
+    };
+  },
+  additionalPaths: async (config) => [
+    await config.transform(config, "/additional-page"),
+  ],
+  robotsTxtOptions: {
+    policies: [
+      {
+        userAgent: "*",
+        allow: "/",
+      },
+      {
+        userAgent: "test-bot",
+        allow: ["/path", "/path-2"],
+      },
+      {
+        userAgent: "black-listed-bot",
+        disallow: ["/sub-path-1", "/path-2"],
+      },
+    ],
+    additionalSitemaps: ["https://fuatcangenc.com/sitemap.xml"],
+  },
+};
+
+export default config;
